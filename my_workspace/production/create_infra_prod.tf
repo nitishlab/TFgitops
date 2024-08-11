d414faea0c79a4cabe6c892c6f0ee75a53881003
Nitish Shishupal <testoranit@gmail.com>
2024-08-11 16:03:23 +0530

    push to prod

--- my_workspace/production/create_infra_prod.tf
+++ my_workspace/production/create_infra_prod.tf
@@ -13,9 +13,11 @@ module "ec2_instance" {
   var_az = ["dummy_value"]
   var_idle_time_out = 60
   var_connection_draining_timeout = 300
+  instance_name = "production-ec2"
 }
 
 ##Prod instance
+##
 ##
 ##
 ##
